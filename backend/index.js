f41d4031100776af97ee3b87ba0d1a80d0909472
Vaibhav Shukla <vaibhavshuklamdi@gmail.com>
2025-06-14 17:39:43 +0530

    Fix cors

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ dotenv.config({})
 
 
 app.use(cors( {
-  origin:  ['http://localhost:5173', 'https://instagram-clone-two-psi.vercel.app'],
+  origin: process.env.FRONTEND_URL,
   credentials: true, 
 }));
 app.use(cookieParser());
